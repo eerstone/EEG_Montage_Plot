582d94af2aef5959ae803fa5aa97d9218c63c091
eerstone <pgd347715204@163.com>
2023-12-07 15:50:49 +0800

    init the repository

---
+++ data_load.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# @Author: eerstone
+# @Date:   2023-12-07 11:39:21
+# @Last Modified by:   eerstone
+# @Last Modified time: 2023-12-07 15:46:24
+import numpy as np
+import mne
+import scipy.io as scio
+
+
+def read_dat(filepath=None):
+    '''
+    读取Neuroscan的dat文件的Sensor Location
+    '''
+    if filepath == None:
+        filepath = "Scan-SynAmps2-Quik-Cap64\\SynAmps2 Quik-Cap64NoM1M2.DAT"
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+    electrodes = {}
+    nasion = lpa = rpa = None
+    for i, line in enumerate(lines):
+        items = line.split()
+        if not items:
+            continue
+        elif len(items) != 4:
+            raise ValueError("ERROR reading dat ERROR")
+        pos = np.array([float(item) for item in items[1:]])
+        if i == 0:
+            nasion = pos
+        elif i == 1:
+            lpa = pos
+        elif i == 2:
+            rpa = pos
+        elif items[0] == "VEO":
+            break
+        else:
+            electrodes[items[0].upper()] = pos
+    return mne.channels.make_dig_montage(electrodes, nasion, lpa, rpa)
+
+
+def read_labelmat(filepath):
+    '''
+    从dpa.mat文件中导出sensorpos
+    '''
+    data = scio.loadmat(filepath)
+    o_labels = data["labels"][0]
+    labels = []
+    for i in o_labels:
+        val = i[0]
+        val = val.upper()
+        labels.append(val)
+    labels = labels[:-1]
+    return labels
+
+
+def read_montagemat(filepath=None):
+    '''
+    从curry8 dpa文件中的sensorpos 和label生成montage
+    filepath 为自己导出的sensorpos 和 labels的合并mat'
+    导出时去掉m1 m2
+    '''
+    if filepath == None:
+        filepath = "Scan-SynAmps2-Quik-Cap64\\Quik-Cap Neo.mat"
+    data = scio.loadmat(filepath)
+    labels = read_labelmat(filepath)
+    sensorpos = data["sensorpos"]
+    sensorpos = np.transpose(sensorpos)
+    # 暂时照抄read_dat中的nasion，lpa与rpa
+    nasion = np.array([0.100031, 9.518287, 0])
+    lpa = np.array([-6.902262, 0, 0])
+    rpa = np.array([6.902262, 0, 0])
+    channels = {}
+    for i in range(64):
+        key = labels[i]
+        sensorpos[i][1] = -sensorpos[i][1]
+        # 暂不清楚之前为啥 / 10 是原始数据比较大？
+        channels[key] = sensorpos[i] / 10
+    ref = ["M1", "M2"]
+    labels.remove("M1")
+    labels.remove("M2")
+    for r in ref:
+        if r in channels:
+            del channels[r]
+    return mne.channels.make_dig_montage(channels, nasion, lpa, rpa)
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ montage_plot.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+# @Author: eerstone
+# @Date:   2023-12-07 10:30:35
+# @Last Modified by:   eerstone
+# @Last Modified time: 2023-12-07 15:47:37
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+def load_montage(montage_filepath=None, readfunc=None):
+    from data_load import read_dat, read_montagemat
+    if readfunc == None:
+        readfunc = read_dat
+    montage = readfunc(montage_filepath)
+    return montage
+
+
+def plot_montage_text(ax, ch_names, pos, show_names):
+    '''
+    基于给定的通道，显示对应名称
+    '''
+    if isinstance(show_names, (list, np.ndarray)):  # only given channels
+        indices = [list(ch_names).index(name) for name in show_names]
+    else:  # all channels
+        indices = range(len(pos))
+    for idx in indices:
+        this_pos = pos[idx]
+        x_offset = 0
+        y_offset = 0.7
+        # if kind == '3d':
+        #     ax.text(this_pos[0], this_pos[1], this_pos[2], ch_names[idx])
+        # else:
+        ## 针对SynAmps2 Quik-Cap64模板的微调
+        if ch_names[idx] in ['AF3', 'FT7']:
+            x_offset -= 0.9
+            y_offset -= 0.7
+        elif ch_names[idx] in ['AF4', 'FT8']:
+            x_offset += 0.9
+            y_offset -= 0.7
+        elif ch_names[idx] in ['PO7', 'PO8']:
+            # x_offset += 0.7
+            y_offset -= 1.4
+        ax.text(this_pos[0] + x_offset,
+                this_pos[1] + y_offset,
+                ch_names[idx],
+                fontsize=13,
+                fontstyle='normal',
+                fontweight='semibold',
+                ha='center',
+                va='center')
+            
+
+def plot_2dmontage(channel_data,  title=None, cmap_name='RdBu_r'):
+    '''
+    给定channel data 输出其数据对应的电极图
+    '''
+    montage = load_montage()
+    # show_names If an array, only the channel names in the array are shown
+    fig = montage.plot(scale_factor=100,
+                       kind='topomap',
+                       show_names=False,
+                       show=False,
+                       sphere=(0.5, 0, 0, 9))
+    ax = fig.axes[0]
+    # get collection of dots in the original montage plot
+    collection = ax.collections[0]
+    
+    map_vir = plt.cm.get_cmap(cmap_name)
+    channel_data_min = channel_data.min()
+    # Two slope
+    if cmap_name == "RdBu_r":
+        if channel_data_min > 0:
+            channel_data_min = -0.000000001
+        norm = mpl.colors.TwoSlopeNorm(vmin=channel_data_min, vcenter=0, vmax=channel_data.max())
+    else:
+        if channel_data.min() < 0:
+            norm = plt.Normalize(channel_data_min, channel_data.max())
+        else:
+            norm = plt.Normalize(0, channel_data.max())
+    if False: # Sometimes maybe needs boundary Norm
+        norm = mpl.colors.BoundaryNorm([-0.5,0.5,1.5,2.5,3.5,4.5,5.5], map_vir.N)  # Permutation Test
+    
+    color = map_vir(norm(channel_data))
+    collection.set_color(color)
+    
+    sm = plt.cm.ScalarMappable(cmap=map_vir, norm=norm)
+    cbar = fig.colorbar(mappable=sm,
+                 ax=ax,
+                 orientation='vertical',
+                 fraction=0.04,
+                 pad=0,
+                 spacing='uniform',)
+
+    # plot montage text based on the condition, heare plot over the half of absolute value
+    pos = collection.get_offsets()
+    ch_names = montage.ch_names
+    nd_ch_names = np.array(ch_names)
+    mask_names = np.abs(channel_data) > np.abs(np.mean(channel_data))
+    show_names = nd_ch_names[mask_names]
+    plot_montage_text(ax=ax, ch_names=ch_names, pos=pos, show_names=show_names)
+    
+    if title == None:
+        fig.suptitle("")
+    else:
+        fig.suptitle(title, fontsize=25)
+    return fig, ax
+
+
+def main():
+    # Example data
+    channel_data = np.random.randn((62))
+    # Example cmaps "viridis", "Reds", "Blues", "RdBu","RdBu_r"
+    fig, ax = plot_2dmontage(channel_data,"Example", cmap_name="Reds")
+    # fig.savefig("fig.eps",format="pdf", bbox_inches='tight')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
